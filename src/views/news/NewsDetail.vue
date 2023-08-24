44bf77301fb13cab4ec55a2577b8daf6840c51c7
Phuc Nguyen <ngqphuc2011@gmail.com>
2023-08-24 21:10:58 +0700

    Reformat

--- src/views/news/NewsDetail.vue
+++ src/views/news/NewsDetail.vue
@@ -11,8 +11,8 @@ const md = new MarkdownIt();
 const item = ref({}) as any
 const itemId = route.params.id
 fetch(apiUrl + '/api/news/' + itemId + '?populate=*').then(response => response.json()).then(res => {
-const content = md.render(res.data.content);
-    item.value = {...res.data, content};
+    const content = md.render(res.data.content);
+    item.value = { ...res.data, content };
 })
 </script>
 <template>
@@ -20,10 +20,10 @@ const content = md.render(res.data.content);
         <v-col cols="12" md="12">
             <UiParentCard :title="item.title">
                 <div class="pa-7 pt-1">
-                        <p class="text-body-2 mb-2 tw-text-gray-500">{{ item.summary }}</p>
-                        <v-img v-if="item.img" :src="apiUrl + item.img.url" height="100"> </v-img>
+                    <p class="text-body-2 mb-2 tw-text-gray-500">{{ item.summary }}</p>
+                    <v-img v-if="item.img" :src="apiUrl + item.img.url" height="100"> </v-img>
                     <p v-html="item.content" class="text-body-1 mb-6 tw-whitespace-pre-line" />
-                    <RouterLink :to="{name: 'News'}" class="tw-underline tw-cursor-pointer">Back to list</RouterLink>
+                    <RouterLink :to="{ name: 'News' }" class="tw-underline tw-cursor-pointer">Back to list</RouterLink>
                 </div>
             </UiParentCard>
         </v-col>

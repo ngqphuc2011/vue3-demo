bae57914b5128ac128e620d2752d00f7633ba377
Nguyen Quang Phuc <nguyen.quang.phuc@linecorp.com>
2023-10-15 12:53:31 +0700

    Add delete butoon

--- src/views/news/NewsDetail.vue
+++ src/views/news/NewsDetail.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 import MarkdownIt from 'markdown-it'
+import { router } from '@/router';
 const apiUrl = import.meta.env.VITE_API_URL
 const route = useRoute()
 const md = new MarkdownIt();
@@ -14,6 +15,15 @@ fetch(apiUrl + '/api/news/' + itemId + '?populate=*').then(response => response.
     const content = md.render(res.data.content);
     item.value = { ...res.data, content };
 })
+const onClickDeleteButton = () => {
+    fetch(apiUrl + '/api/news/' + itemId, {
+        method: 'DELETE',
+    }).then(() => {
+        router.push({
+            name: 'News',
+        })
+    })
+}
 </script>
 <template>
     <v-row>
@@ -23,7 +33,10 @@ fetch(apiUrl + '/api/news/' + itemId + '?populate=*').then(response => response.
                     <p class="text-body-2 mb-2 tw-text-gray-500">{{ item.summary }}</p>
                     <v-img v-if="item.img" :src="apiUrl + item.img.url" class="mb-2" height="400"> </v-img>
                     <p v-html="item.content" class="text-body-1 mb-6 tw-whitespace-pre-line" />
-                    <RouterLink :to="{ name: 'News' }" class="tw-underline tw-cursor-pointer">Back to list</RouterLink>
+                    <div class="tw-flex tw-justify-between">
+                        <RouterLink :to="{ name: 'News' }" class="tw-underline tw-cursor-pointer">Back to list</RouterLink>
+                        <span class="tw-underline tw-cursor-pointer" @click="onClickDeleteButton">Delete</span>
+                    </div>
                 </div>
             </UiParentCard>
         </v-col>

ab8306eac84f6a6abc94a3346ffcd0d0fa157e21
Nguyen Quang Phuc <nguyen.quang.phuc@linecorp.com>
2023-10-17 10:33:08 +0700

    Fix css

--- src/components/shared/UiParentCard.vue
+++ src/components/shared/UiParentCard.vue
@@ -10,7 +10,7 @@ const props = defineProps({
     <v-card elevation="10" class="withbg">
         <v-card-item class="pa-0">
             <div class="d-sm-flex align-center justify-space-between">
-                <h1 class="text-h5 mb-6 pl-7 pt-7">{{ title }}</h1>
+                <h1 class="text-h5 mb-6 px-7 pt-7 tw-truncate">{{ title }}</h1>
                 <!-- <template v-slot:append> -->
                 <slot name="action"></slot>
                 <!-- </template> -->
